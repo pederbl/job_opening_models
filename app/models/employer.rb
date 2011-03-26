9808262949e69a5cd7c9db36432fd83fabb08ab8
Peder Linder <pederbl@gmail.com>
2011-03-26 07:49:18 +0000

    a

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,7 +1,17 @@
 class Employer
   include Mongoid::Document
+  include Mongoid::TranslatedStrings
   embedded_in :job_opening, reverse_of: :employer
 
   field :name
+  field :national_id
+  field :email
+  field :phone
+  field :fax
+  field :website
+  field :mail_address
+  field :visit_address
+  field :text, type: Hash
+  #field :staffing_agency, type: Boolean
 
 end
--- app/models/job_opening.rb
+++ app/models/job_opening.rb
@@ -1,42 +1,46 @@
 class JobOpening
   include Mongoid::Document
+  include Mongoid::TranslatedStrings
 
   field :listing_id, type: Integer
   field :updated_at, type: DateTime
   field :deleted_at, type: DateTime
-  field :publish_on, type: Date
-  #field :publish_until, type: Date
+  field :publish_at, type: DateTime
+  field :publish_until, type: DateTime
 
   field :source 
   field :source_id
   field :source_url
 
-  field :title
-  field :body
-  #field :seasons, type: Integer, default: 0
-  #field :num_positions, type: Integer, default: 1
-  #field :co_founder, type: Boolean, default: false
-  #field :contractor, type: Boolean, default: false
-  #field :original_language
+  field :title, type: Hash
+  field :body, type: Hash
+  field :num_positions, type: Integer, default: 1
+  field :primary_language, default: 'x'
 
   #field :industry_tags, type: Array
-  #field :job_category_tags, type: Array
-  #field :position_title
+  field :job_category_tags, type: Array
+  field :isco, type: Integer
+  field :nace, type: Integer
+  field :job_title, type: Hash
+  field :job_type  # e.g. contractor, cofounder, internship, trainee
 
   embeds_one :employer
-  #embeds_one :requirements, class_name: "Requirements"
-  #embeds_one :worktime
-  #embeds_one :duration
-  #embeds_one :salary
-  #embeds_one :application
+  embeds_one :requirements, class_name: "Requirements"
+  embeds_one :worktime
+  embeds_one :duration
+  embeds_one :salary
+  embeds_one :application
+  embeds_one :location
 
-  embeds_many :locations
-  #embeds_many :contacts
-  #embeds_many :union_contacts, class_name: "Contact"
-
-  #embeds_many :translations
+  embeds_many :contacts
+  embeds_many :union_contacts, class_name: "Contact"
 
   validates :updated_at, presence: true
   validates :source, presence: true
   validates :source_id, presence: true
+
+  scope :active, where: { deleted_at: nil }
+
 end
+
+
